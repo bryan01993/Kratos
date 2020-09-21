c66e1e3f309bb07426be727b1eb5f1e227a2fb9e
Miguel Figueira <miguel.figueira16@gmail.com>
2020-09-21 23:11:00 +0200

    Refactor HCInit

--- ProductionOpti.py
+++ ProductionOpti.py
@@ -785,8 +785,8 @@ def AccotateOptisetsPhase2(): #Still not decided if this STEP SHOULD BE INCLUDED
 
 
 # =============================================================================================== HILL CLIMBING INIT FILE ==========================================================================================
-def HCInit (PairList ='EURUSD',TimeFrameList ='H1',OptimizationCriterionList=0,ModelList=2,OptimizationList=1,ShutdownTerminalList=1,VisualList=0,LeverageValue=33,ReplaceReportList=1,UseLocalList=1,ForwardModeList=0,ExecutionValue=28):
-    f =open(FOLDER_PATH +'/reports/{}/INITS/HC/'.format(BotName.get()) + 'INIT-HC-{}-{}-{}-Phase1.ini'.format(BotName.get(),PairList,TimeFrameList),'w')
+def HCInit (pair ='EURUSD', time_frame ='H1', optimization_criterion=0, model=2, optimization=1, shutdown=1, visual=0, leverage=33, replace_report=1, use_local=1, forward_mode=0, execution_mode=28):
+    f =open(FOLDER_PATH +'/reports/{}/INITS/HC/'.format(BotName.get()) + 'INIT-HC-{}-{}-{}-Phase1.ini'.format(BotName.get(), pair, time_frame),'w')
     f.write(';[Common]' + "\n" \
     ';Login=40539843' + "\n" \
     ';Password=jPHIWVnmZUFn' + "\n"  \
@@ -799,30 +799,30 @@ def HCInit (PairList ='EURUSD',TimeFrameList ='H1',OptimizationCriterionList=0,M
     '[Tester]' + "\n" \
     'Expert=Advisors\{}'.format(BotName.get()) + "\n" \
     'ExpertParameters= HCB-Phase1-{}.set'.format(BotName.get()) + "\n" \
-    'Symbol={}'.format(PairList) + 'MT5' + "\n" \
-    'Period={}'.format(TimeFrameList) + "\n" \
+    'Symbol={}'.format(pair) + 'MT5' + "\n" \
+    'Period={}'.format(time_frame) + "\n" \
     ';Login=XXXXXX' + "\n" \
-    'Model={}'.format(ModelList) + "\n" \
-    'ExecutionMode={}'.format(str(ExecutionValue)) + "\n" \
-    'Optimization={}'.format(OptimizationList) + "\n" \
-    'OptimizationCriterion={}'.format(OptimizationCriterionList) + "\n" \
+    'Model={}'.format(model) + "\n" \
+    'ExecutionMode={}'.format(str(execution_mode)) + "\n" \
+    'Optimization={}'.format(optimization) + "\n" \
+    'OptimizationCriterion={}'.format(optimization_criterion) + "\n" \
     'FromDate={}'.format(Opti_start_date.get()) + "\n" \
     'ToDate={}'.format(Opti_end_date.get()) + "\n" \
-    ';ForwardMode={}'.format(ForwardModeList) + "\n" \
+    ';ForwardMode={}'.format(forward_mode) + "\n" \
     ';ForwardDate={}'.format(ForwardDate.get()) + "\n" \
-    'Report=reports\{}\INITS\HC-Phase1-{}-{}-{}'.format(BotName.get(),BotName.get(),PairList,TimeFrameList) + "\n" \
+    'Report=reports\{}\INITS\HC-Phase1-{}-{}-{}'.format(BotName.get(), BotName.get(), pair, time_frame) + "\n" \
     ';--- If the specified report already exists, it will be overwritten' + "\n" \
-    'ReplaceReport={}'.format(ReplaceReportList) + "\n" \
+    'ReplaceReport={}'.format(replace_report) + "\n" \
     ';--- Set automatic platform shutdown upon completion of testing/optimization' + "\n" \
-    'ShutdownTerminal={}'.format(ShutdownTerminalList) + "\n" \
+    'ShutdownTerminal={}'.format(shutdown) + "\n" \
     'Deposit={}'.format(Initial_deposit) + "\n" \
     'Currency={}'.format(Deposit_Currency) + "\n" \
     ';Uses or refuses local network resources' + "\n" \
-    'UseLocal={}'.format(UseLocalList) + "\n" \
+    'UseLocal={}'.format(use_local) + "\n" \
     ';Uses Visual test Mode' + "\n" \
-    ';Visual={}'.format(VisualList) + "\n" \
+    ';Visual={}'.format(visual) + "\n" \
     'ProfitInPips=0' + "\n" \
-    'Leverage={}'.format(str(LeverageValue)) + "\n")
+    'Leverage={}'.format(str(leverage)) + "\n")
     f.close()
     # ---------------------------------------------------PROCESS TO OBTAIN OPTISET VALUES FOR HILL CLIMBING PROCESS---------------------------------
 
