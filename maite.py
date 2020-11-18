690e692c757614b6547a8b85501a3fc431e3c12b
Miguel Figueira <miguel.figueira16@gmail.com>
2020-11-18 20:06:36 +0100

    Update maite

--- maite.py
+++ maite.py
@@ -15,10 +15,14 @@ strategy = le.fit_transform(list(dataframe['Strategy name (Global)']))
 balance = dataframe['Balance (Global)']
 size= dataframe['Size (Global)']
 
-dataframe = list(zip(balance, strategy, size))
+dataframe = pd.DataFrame(list(zip(balance, strategy, size)))
+columns = len(dataframe.columns)
 
-model = tf.keras.Sequential()
-model.add(tf.keras.Input(shape=(3,)))
-model.add(tf.keras.layers.Dense(8))
-model.compile(optimizer="adam",loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=["accuracy"])
+model = keras.Sequential([
+    keras.layers.Dense(32, activation="relu", input_shape=(columns, ), use_bias=False),
+    keras.layers.Dense(4, activation="softmax") # output layer
+])
+model.compile(optimizer="adam", loss=tf.keras.losses.MeanSquaredError(), metrics=["accuracy"])
+model.summary()
+
 model.fit(dataframe, epochs=5)
