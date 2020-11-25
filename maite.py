8873478fa8a04ecb98a8840b06cb30a5ce94a2fa
Bryan Aleixo <bryan.aleixo@gmail.com>
2020-11-25 22:01:12 +0100

    Broken maite

--- maite.py
+++ maite.py
@@ -9,20 +9,22 @@ import pandas as pd
 
 LOCATION = './atrox/WARWICK_Portfolio.csv'
 dataframe = pd.read_csv(LOCATION)
-
 le = preprocessing.LabelEncoder()
 strategy = le.fit_transform(list(dataframe['Strategy name (Global)']))
 balance = dataframe['Balance (Global)']
 size= dataframe['Size (Global)']
-
+print(len(dataframe.columns))
 dataframe = pd.DataFrame(list(zip(balance, strategy, size)))
 columns = len(dataframe.columns)
-
+print(columns)
 model = keras.Sequential([
-    keras.layers.Dense(32, activation="relu", input_shape=(columns, ), use_bias=False),
-    keras.layers.Dense(4, activation="softmax") # output layer
+    keras.layers.Dense(32, activation="relu", input_shape=(columns, ), use_bias=False,name='Input_Layer'),
+    keras.layers.Dense(4, activation="relu",name='Output_Layer') # output layer
 ])
-model.compile(optimizer="adam", loss=tf.keras.losses.MeanSquaredError(), metrics=["accuracy"])
-model.summary()
 
-model.fit(dataframe, epochs=5)
+optimizer = keras.optimizers.SGD(learning_rate=0.01, momentum=0.0, nesterov=False,name='Optimizer_SGD')
+model.compile(optimizer=optimizer, loss=tf.keras.losses.MeanSquaredError(), metrics=["accuracy"])
+model_checkpoint = tf.keras.callbacks.ModelCheckpoint('./checkpoints/',monitor = 'val_accuracy',verbose =2 ,save_weights_only=True,save_best_only=True,mode='max')
+model.summary()
+model.get_weights()
+history = model.fit(dataframe,batch_size=1, epochs=5)
