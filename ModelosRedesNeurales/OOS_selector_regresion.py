a70f1f06560e19fa42622899b44041d05cadbeb0
Miguel Figueira <miguel.figueira16@gmail.com>
2021-08-02 21:02:41 +0200

    Add kerastuner

--- ModelosRedesNeurales/OOS_selector_regresion.py
+++ ModelosRedesNeurales/OOS_selector_regresion.py
@@ -2,18 +2,28 @@ import numpy as np
 import pandas as pd
 import os
 import sys
+import tensorflow as tf
+import kerastuner as kt
+from tensorflow.keras import regularizers
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+from tensorflow.keras import layers
+from kerastuner import RandomSearch
+from keras_tuner import HyperParameters
+from keras_tuner.applications import HyperXception
+
+
+
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-import tensorflow as tf
-from tensorflow.keras import regularizers
-from sklearn.model_selection import GridSearchCV
-from tensorflow.python.keras.wrappers.scikit_learn import KerasRegressor
 from services.create_timebricks import CreateTimebricks
 from services.helpers import movecol
-from sklearn import preprocessing
-import matplotlib.pyplot as plt
+from ModelosRedesNeurales.my_custom_callback import MyCustomCallback
+
+
+
 
 
 ### Rutas a los directorios de data, de guardado de resultados y de tensores para TensorBoard
@@ -99,7 +109,7 @@ class SelectorRegression:
             print("Select a normalization type between Median and MaxMin")
         return raw_dataframe
 
-    def create_basic_model(self, input_dimension):
+    def build_basic_model(self, input_dimension):
         """Here the Model is created"""
         optimizer = 'adam'
         init_mode = 'uniform'
@@ -107,8 +117,7 @@ class SelectorRegression:
         dropout_rate = 0.5
         wd = 1e-5
 
-
-        self.model = tf.keras.Sequential([
+        model = tf.keras.Sequential([
             tf.keras.layers.Dense(1200, input_shape=(input_dimension, ), kernel_regularizer=regularizers.l2(wd), activation=activation, name='First_Layer'),
             tf.keras.layers.Dense(1000, kernel_regularizer=regularizers.l2(wd), activation=activation, name='Second_Layer'),
             tf.keras.layers.Dropout(dropout_rate),
@@ -121,8 +130,66 @@ class SelectorRegression:
             tf.keras.layers.Dense(100, kernel_regularizer=regularizers.l2(wd), activation=activation, name='Fourth_Layer'),
             tf.keras.layers.Dense(1, kernel_initializer=init_mode, name='Fifth_Layer'),
         ])
-        self.model.compile(optimizer=optimizer, loss='mae', metrics='mae')
-        self.model.optimizer.learning_rate.assign(0.001)
+
+        model.compile(optimizer=optimizer, loss='mae', metrics='mae')
+        model.optimizer.learning_rate.assign(0.001)
+
+        return model
+
+    def build_model(self, hp):
+        test_run = self.split_train_test_sequest_bricks()
+        self.train_dataframe = self.concatenate_phase(phase_list = self.train_list, Target="CustomForward")[0]
+        self.train_target = self.concatenate_phase(phase_list = self.train_list, Target="CustomForward")[1]
+        self.norm_train_dataframe = self.normalize_dataframe(self.train_dataframe, 'Median')
+        self.norm_train_target = self.normalize_dataframe(self.train_target, 'Median')
+        validation_dataframe = self.concatenate_phase(phase_list = self.test_list, Target="CustomForward")[0]
+        validation_targets = self.concatenate_phase(phase_list = self.test_list, Target="CustomForward")[1]
+
+        input_dimension=len(self.train_dataframe.columns)
+        model = tf.keras.Sequential()
+
+        activation = 'tanh'
+        wd = 1e-5
+
+        model.add(layers.Dense(1200, input_shape=(input_dimension, ), kernel_regularizer=regularizers.l2(wd), activation=activation, name='First_Layer'))
+
+        for i in range(hp.Int("num_layers", 2, 20)):
+            print("aaaaaaaaaaaaaaaaaaaaaaa")
+
+            model.add(
+                layers.Dense(
+                    units=hp.Int("units_" + str(i), min_value=32, max_value=512, step=32),
+                    activation="relu",
+                )
+            )
+
+        optimizer=tf.keras.optimizers.Adam(hp.Choice("learning_rate", [1e-2, 1e-3, 1e-4])),
+        model.add(layers.Dense(1, activation="tanh"))
+        model.compile(optimizer=optimizer, loss='mae', metrics='mae')
+
+        return model
+
+    def run_tuner(self):
+
+        hp = HyperParameters()
+
+        # This will override the `learning_rate` parameter with your own selection of choices
+        hp.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
+        tuner = RandomSearch(
+            self.build_model,
+            objective="mae",
+            max_trials=3,
+            executions_per_trial=2,
+            overwrite=True,
+            directory="my_dir",
+            project_name="helloworld",
+        )
+
+        validation_dataframe = self.concatenate_phase(phase_list = self.test_list, Target="CustomForward")[0]
+        validation_targets = self.concatenate_phase(phase_list = self.test_list, Target="CustomForward")[1]
+
+        tuner.search(self.train_dataframe, self.train_target, epochs=10, validation_data=(validation_dataframe, validation_targets))
+
 
     def run(self):
         test_run = self.split_train_test_sequest_bricks()
@@ -135,14 +202,14 @@ class SelectorRegression:
         validation_targets = self.concatenate_phase(phase_list = self.test_list, Target="CustomForward")[1]
         print('testing done')
         print('Enter basic model')
-        self.create_basic_model(input_dimension=len(self.train_dataframe.columns))
 
         callback_path = os.path.join(save_dir + 'savedmodel.ckpt')
 
         tensorboard = tf.keras.callbacks.TensorBoard(log_dir=tensor_dir + 'logs', histogram_freq=1)
         model_callbacks = tf.keras.callbacks.ModelCheckpoint(filepath=callback_path, save_best_only=True, verbose=2)
 
-        history = self.model.fit(
+        model = self.build_model(input_dimension=len(self.train_dataframe.columns))
+        history = model.fit(
             x=self.train_dataframe,
             y=self.train_target,
             batch_size=5000,
@@ -153,7 +220,6 @@ class SelectorRegression:
             callbacks=[tensorboard, model_callbacks]
         )
 
-        ### Plots the Training and Validation
         plt.figure(1)
         plt.plot(np.sqrt(history.history['loss']))
         plt.plot(np.sqrt(history.history['val_loss']))
@@ -165,4 +231,6 @@ class SelectorRegression:
 
 
 smth = SelectorRegression(train_start, test_start, sequest_start, train_steps)
-smth.run()
+# smth.run()
+smth.run_tuner()
+
---
+++ epoch.md
@@ -0,0 +1,22 @@
+## Epoch
+Tipos de funciones de perdidas
+
+
+### Entrenamiento
+- loss
+- mae (Mean Absolute Error)
+
+
+### Validación
+- val_loss
+- val_mae:
+
+
+### Cual es el epoch ideal
+- Menor error de validación posible, (val_loss y val_mae) y el entrenamiento también debe ser bajo por consecuencia.
+
+
+## TO-DO
+Encontrar el resultado de validación cada epoch. --- check!
+
+
